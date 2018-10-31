aa7ea0a1bc54e6d642ef3e955716d0a6e0f43ec5
nmendoza <vmend001@odu.edu>
2018-10-31 12:59:02 -0400

    Separating footer code into js file, for single use across all pages

---
+++ website/js/loader.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    var footerHTML = "<!-- ***** Footer Area Start ***** -->\n" +
+        "<footer class=\"footer-social-icon text-center section_padding_70 clearfix\">\n" +
+        "    <!-- footer logo -->\n" +
+        "    <div class=\"footer-text\">\n" +
+        "        <h4>CS 410 | Team Silver</h4>\n" +
+        "    </div>\n" +
+        "    <!-- Foooter Text-->\n" +
+        "    <div class=\"copyright-text\">\n" +
+        "        <!-- ***** Removing this text is now allowed! This template is licensed under CC BY 3.0 ***** -->\n" +
+        "        <p>Copyright ©2017 Ca. Designed by <a href=\"https://colorlib.com\" target=\"_blank\">Colorlib</a></p>\n" +
+        "    </div>\n" +
+        "</footer>";
+
+    $('footer').html(footerHTML);
+
+});
+
